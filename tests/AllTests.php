b4e1ccafb74ee25a7e0ee6a9ba64049ca0c8c43c
Markus Tacker <m@tacker.org>
2011-11-15 10:07:58 +0100

    Fixes PEAR Bug #19001.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -31,7 +31,6 @@
     * @version $Id$
     */
 
-    error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
     require_once 'tests/FileBittorrent2.php';
--- tests/Bug16148.php
+++ tests/Bug16148.php
@@ -35,7 +35,6 @@
 require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'File/Bittorrent2/Encode.php';
 
-error_reporting( E_ALL | E_STRICT );
 ini_set( 'display_errors', 1 );
 ini_set( 'log_errors', 0 );
 
